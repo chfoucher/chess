7e7d871466535af76d135ab1db6cadab54869e45
chfoucher <pele.foucher@free.fr>
2021-01-03 16:45:56 +0100

    Utilise json_encode

--- history/get.php
+++ history/get.php
@@ -1,7 +1,15 @@
 <?php
-$test = array('a');
+$origin = array(1, 2);
+$destination = array(4, 3);
+$move = array(origin => $origin, destination => $destination);
+$history = array();
+$history[0] = $move;
+$origin = array(7, 0);
+$destination = array(6, 4);
+$move = array(origin => $origin, destination => $destination);
+$history[1] = $move;
 
-$data = '{"data": [{"origin": {"r": 1, "c": 2}, "destination": {"origin": 4, "destination": 3}}]}';
+$data = json_encode($history);
 header('Content-type: application/json');
 echo $data;
 ?>
