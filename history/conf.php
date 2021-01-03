3e5777892a8cfa4df47c725a330a9f09062abe1f
chfoucher <pele.foucher@free.fr>
2021-01-03 17:52:13 +0100

    Ajout base de données mySQL

---
+++ history/conf.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$user = "pele.foucher";
+$password = "rusacxf9";
+#$database = "divers";
+?>
--- history/get.php
+++ history/get.php
@@ -1,13 +1,26 @@
 <?php
-$origin = array(1, 2);
-$destination = array(4, 3);
-$move = array(origin => $origin, destination => $destination);
+require("conf.php");
+
+$mysqli = new mysqli($host, $user, $password);
+if (mysqli_connect_errno()) {
+    printf("Échec de la connexion : %s\n", mysqli_connect_error());
+    exit();
+}
+
 $history = array();
-$history[0] = $move;
-$origin = array(7, 0);
-$destination = array(6, 4);
-$move = array(origin => $origin, destination => $destination);
-$history[1] = $move;
+
+if ($result = $mysqli->query("SELECT * FROM chess")) {
+	$i = 0;
+   while ($row = $result->fetch_row()) {
+		  $origin = array(intval($row[1]), intval($row[2]), intval($row[3]), intval($row[4]));
+		  $destination = array(intval($row[5]), intval($row[6]), intval($row[7]), intval($row[8]));
+		  $move = array(origin => $origin, destination => $destination);
+		  $history[intval($row[0])] = $move;
+   }
+	$result->close();
+} else printf("Query has failed\n");
+
+$mysqli->close();
 
 $data = json_encode($history);
 header('Content-type: application/json');
