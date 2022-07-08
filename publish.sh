a85c86d48212844a2f24531e878ff5bb6fd1c27c
chfoucher <pele.foucher@free.fr>
2022-07-08 11:04:53 +0200

    Add HTML and javascript files to publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 WWW="/var/www/html/chess"
+
 mkdir -p $WWW/history
 cp history/conf.php $WWW/history
 cp history/get.php $WWW/history
 
+mkdir -p $WWW/scripts
+cp scripts/main.js $WWW/scripts
+
+cp index.html $WWW
