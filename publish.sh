ae466a9d926c99470aa2ff1a8f9c1e8192fa4b77
chfoucher <pele.foucher@free.fr>
2022-07-19 18:57:02 +0200

    Ajout publication remote

--- publish.sh
+++ publish.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
-WWW="/var/www/html/chess"
 
-mkdir -p $WWW/history
-cp history/conf.php $WWW/history
-cp history/get.php $WWW/history
+publishLocally() {
+    WWW="/var/www/html/chess"
 
-mkdir -p $WWW/scripts
-cp scripts/main.js $WWW/scripts
+    mkdir -p $WWW/history
+    cp history/conf.php $WWW/history
+    cp history/get.php $WWW/history
 
-cp index.html $WWW
+    mkdir -p $WWW/scripts
+    cp scripts/main.js $WWW/scripts
+
+    cp index.html $WWW
+}
+
+publishRemotely() {
+    echo "Publish to $1 by FTP"
+    ftp -i ftpperso.free.fr <<EOF
+    cd Christophe/chess
+    put index.html
+    put history/get.php
+    put scripts/main.js
+EOF
+
+}
+
+if [ -z $1 ]
+then
+    publishLocally
+else
+    publishRemotely $1
+fi
+exit 0
